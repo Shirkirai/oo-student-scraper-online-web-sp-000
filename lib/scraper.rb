78681b93b7d4906b45db79cca4cdfb34923ae097
Jung Lee <lee.jung.s95@gmail.com>
2020-08-24 01:59:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -30,7 +30,7 @@ class Scraper
       students_hash[:blog] = url if doc.css("img").attribute("src").text.include?("rss")
     end
       students_hash[:profile_quote] = doc.css("div.profile-quote").text
-      students_hash[:bio] = doc.css("div.bio-content.content-holder p").text
+      students_hash[:bio] = doc.css("div.bio-content.content-holder div.description-holder p").text
 
     students_hash
   end

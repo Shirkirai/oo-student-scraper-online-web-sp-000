0718b4528b5c4eee3bdb67aaeb2bb6e40f762419
Jung Lee <lee.jung.s95@gmail.com>
2020-08-24 00:41:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,7 +29,7 @@ class Scraper
         profile_url = "#{student.attr("href")}"
         students << {name: name, location: location, profile_url: profile_url}
       end
-    
+
     end
       students
   end

adf76336363ea394c3ef63b475362628ed787e40
Jung Lee <lee.jung.s95@gmail.com>
2020-08-24 01:58:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,7 +27,7 @@ class Scraper
       students_hash[:twitter_url] = url if url.include?("twitter")
       students_hash[:linkedin_url] = url if url.include?("linkedin")
       students_hash[:github_url] = url if url.include?("github")
-      students_hash[:blog_url] = url if doc.css("img").attribute("src").text.include?("rss")
+      students_hash[:blog] = url if doc.css("img").attribute("src").text.include?("rss")
     end
       students_hash[:profile_quote] = doc.css("div.profile-quote").text
       students_hash[:bio] = doc.css("div.bio-content.content-holder p").text

6e79a8013d3f94f3a8790a5c14f56f0e9e33fdc1
Jung Lee <lee.jung.s95@gmail.com>
2020-08-24 00:28:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,29 +19,6 @@ class Scraper
     end
     students_hash
   end
-    #html = open(index_url)
-    #doc = Nokogiri::HTML(html)
-    #names = doc.css(".student-card .card-text-container .student-name")
-    #student_name = names.each do |name|
-    # => puts name.text
-    # end
-
-    #hash = {
-    #  name = scraped data,
-    #  more = more data
-    #}
-    #Student.new(hash)
-    #name = scraped data
-    #more = more data
-    #Student.new(name, more)
-
-    #locations = doc.css(".student-card .card-text-container .student-location").text.strip
-    #student_location = locations.each do |location|
-    # puts location.text
-    #end
-
-    #
-  end
 
   def self.scrape_profile_page(profile_url)
 

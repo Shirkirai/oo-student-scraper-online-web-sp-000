407519fe25504ec80867709ed07612b84777ffca
Jung Lee <lee.jung.s95@gmail.com>
2020-08-24 00:54:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,20 +4,6 @@ require 'pry'
 
 class Scraper
 
-  #def self.scrape_index_page(index_url)
-
-  #  students_hash = []
-  #  html = Nokogiri::HTML(open(index_url))
-  #  html.css(".student-card").collect do |student|
-  #    students_hash << {
-  #      name: student.css("h4.student-name").text,
-  #      location: student.css("p.student-location").text,
-  #      profile_url: "http://students.learn.co/" + "#{student.css("a").attribute("href")}"
-  #    }
-#
-  #  end
-  #  students_hash
-  #end
   def self.scrape_index_page(index_url)
 
     doc = Nokogiri::HTML(open(index_url))
